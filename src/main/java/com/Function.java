3b4e577441cc51405c14d97590db47e0d7df782e
timmyreilly <timreilly@live.com>
2019-08-08 15:50:04 -0700

    adding code for forwarding messages to potential subscribers

--- src/main/java/com/Function.java
+++ src/main/java/com/Function.java
@@ -1,6 +1,12 @@
 package com;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 
@@ -8,20 +14,54 @@ import com.microsoft.azure.functions.*;
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke
-     * it using "curl" command in bash: 1. curl -d "HTTP Body" {your
-     * host}/api/HttpTrigger-Java&code={your function key} 2. curl "{your
-     * host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
-     * Function Key is not needed when running locally, it is used to invoke
-     * function deployed to Azure. More details:
-     * https://aka.ms/functions_authorization_keys
-     */
+
+    private static HttpURLConnection connection;
+
     @FunctionName("ServiceBusQueueTrigger-Java")
     public void run(
             @ServiceBusQueueTrigger(name = "message", queueName = "mvnDemoQueue", connection = "StorageToIndexer_SERVICEBUS") String message,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
+
+
+        String defaultEndpoint = "https://prod-18.westus2.logic.azure.com:443/workflows/faed76bb5c43460186a9ae81cac85c80/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=oDHW9FrpLWUMbF86kudup5ANyUbz6QFQ0HHb-nShxeI";
+        String endpoint = System.getenv("URL_ENDPOINT") == null ? defaultEndpoint : System.getenv("URL_ENDPOINT");
+
+        String url = endpoint;
+        byte[] postData = message.getBytes(StandardCharsets.UTF_8);
+
+        try {
+            URL myUrl = new URL(url);
+            connection = (HttpURLConnection) myUrl.openConnection();
+
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.write(postData);
+            }
+
+            StringBuilder content;
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+
+                String line;
+                content = new StringBuilder();
+
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+
+            context.getLogger().info(content.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
 
         context.getLogger().info("Java Service Bus Queue trigger function executed.");
         context.getLogger().info(message);
